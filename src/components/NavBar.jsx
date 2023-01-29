d2f1b14e701cd52b7e4d2ab5dcd4fed3bc39cf1f
jagerziel <fromnytosg@gmail.com>
2023-01-29 17:01:47 -0500

    Updated Navbar Styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,5 @@
+// Import React-Router-Dom
+import { Link } from "react-router-dom";
 // Import Components
 
 // Import CSS
@@ -9,8 +11,27 @@ export default function NavBar() {
   // Return Div for NavBar Container
   return (
     <div className="NavBar-container">
-      <div>
-        NavBar 
+      <div className="NavBar-left">
+      <div className="NavBar-name">
+            Ryan Ehrlich
+        </div>
+        <div className="NavBar-title">
+            Software Engineer
+        </div>
+      </div>
+      <div className="NavLinks-container">
+        <Link to="/" className="Link">
+            Home
+        </Link>
+        <Link to="/About" className="Link">
+            About
+        </Link>
+        <Link to="/Portfolio" className="Link">
+            Portfolio
+        </Link>
+        <Link to="/Contact" className="Link">
+            Contact
+        </Link>
       </div>
     </div>
   );
