4fc1a93de573cd1941a3d85d6f75914cfd2148d5
jagerziel <fromnytosg@gmail.com>
2023-02-01 06:10:01 -0500

    Biography snapshot done - for now

--- src/components/About/AboutMe.jsx
+++ src/components/About/AboutMe.jsx
@@ -25,9 +25,9 @@ export default function AboutMe() {
             <img className="profile-img" src="images/pic_placeholder.jpg" alt="Img" />
           </div>
           <div className="AboutMe-Right">
-            <h1>
+            <h2>
               An Innovative Mindset that Brings Projects to New Heights
-            </h1>
+            </h2>
             <p id="italics">I'm an innovator with a passion for business and tech.  Here are some of my passions: </p>
             <div className="AboutMe-List">
               <ul>
@@ -56,10 +56,20 @@ export default function AboutMe() {
                 <li className="amli">
                   <div><img id="img1" className="profile-img" src="images/SustIcon.png" alt="Img" /></div>
                   <div>Achieving the Impossible</div>
-                  
-                  
                 </li>
               </ul>
+            </div>
+            <div className="AboutMe-Desc">
+              Experience in front-end and back-end development. A highly analytical, innovative, and strategic thinker who formulates and implements unique solutions to complex problems. As a former multinational insurance broking and consulting professional, I approach software development with a keen perspective on business application as well as current and future needs. I have worked with complex multinationals in the Education, Food and Beverage, Financial, Real Estate, and Retail sectors as well as domestic Renewable Energy.
+            </div>
+            <div className="AboutMe-Desc">
+              My passion is creating things that have never been built before and I blueprint to ensure the creation can evolve, maintain sustainable upkeep costs, and foster easy adoption.   
+            </div>
+            <div className="AboutMe-Desc">
+              Always innovating, always sharing ideas, and always pushing the limits.
+            </div>
+            <div className="AboutMe-LBSS">
+              Let's build something special!
             </div>
           </div>
 
