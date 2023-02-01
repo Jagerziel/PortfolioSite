d688fa6e26d65575a248f61f3cb77ae97dd2ed51
jagerziel <fromnytosg@gmail.com>
2023-02-01 10:19:32 -0500

    Icons added for SE Skills

--- src/components/About/SESkills.jsx
+++ src/components/About/SESkills.jsx
@@ -23,67 +23,67 @@ export default function SESkills() {
             </div>
             <div className="IconBox">
                 <div className="SEli">
-                    <div><img id="img2" className="SES-img" src="images/HTMLIcon.png" alt="Img" /></div>
+                    <div><img id="img2" className="SES-img" src="images/CSSIcon.png" alt="Img" /></div>
                     <div className="whiteText">CSS</div>
                 </div>
             </div>
             <div className="IconBox">
                 <div className="SEli">
-                    <div><img id="img3" className="SES-img" src="images/HTMLIcon.png" alt="Img" /></div>
+                    <div><img id="img3" className="SES-img" src="images/JSIcon.png" alt="Img" /></div>
                     <div className="whiteText">JavaScript</div>
                 </div>
             </div>
             <div className="IconBox">
                 <div className="SEli">
-                    <div><img id="img4" className="SES-img" src="images/HTMLIcon.png" alt="Img" /></div>
+                    <div><img id="img4" className="SES-img" src="images/PythonIcon.png" alt="Img" /></div>
                     <div className="whiteText">Python3</div>
                 </div>
             </div>
             <div className="IconBox">
                 <div className="SEli">
-                    <div><img id="img5" className="SES-img" src="images/HTMLIcon.png" alt="Img" /></div>
+                    <div><img id="img5" className="SES-img" src="images/NodeJSIcon.png" alt="Img" /></div>
                     <div className="whiteText">Node.js</div>
                 </div>
             </div>
             <div className="IconBox">
                 <div className="SEli">
-                    <div><img id="img6" className="SES-img" src="images/HTMLIcon.png" alt="Img" /></div>
+                    <div><img id="img6" className="SES-img" src="images/ExpressIcon.png" alt="Img" /></div>
                     <div className="whiteText">Express</div>
                 </div>
             </div>
             <div className="IconBox">
                 <div className="SEli">
-                    <div><img id="img7" className="SES-img" src="images/HTMLIcon.png" alt="Img" /></div>
+                    <div><img id="img7" className="SES-img" src="images/ReactIcon.png" alt="Img" /></div>
                     <div className="whiteText">React</div>
                 </div>
             </div>
             <div className="IconBox">
                 <div className="SEli">
-                    <div><img id="img8" className="SES-img" src="images/HTMLIcon.png" alt="Img" /></div>
+                    <div><img id="img8" className="SES-img" src="images/MongooseIcon.png" alt="Img" /></div>
                     <div className="whiteText">Mongoose</div>
                 </div>
             </div>
             <div className="IconBox">
                 <div className="SEli">
-                    <div><img id="img9" className="SES-img" src="images/HTMLIcon.png" alt="Img" /></div>
+                    <div><img id="img9" className="SES-img" src="images/DBIcon.png" alt="Img" /></div>
                     <div className="whiteText">MongoDB</div>
                 </div>
             </div>
             <div className="IconBox">
                 <div className="SEli">
-                    <div><img id="img10" className="SES-img" src="images/HTMLIcon.png" alt="Img" /></div>
+                    <div><img id="img10" className="SES-img" src="images/SQLIcon.png" alt="Img" /></div>
                     <div className="whiteText">MySQL</div>
                 </div>
             </div>
             <div className="IconBox">
                 <div className="SEli">
-                    <div><img id="img11" className="SES-img" src="images/HTMLIcon.png" alt="Img" /></div>
+                    <div><img id="img11" className="SES-img" src="images/CIcon.png" alt="Img" /></div>
                     <div className="whiteText">C</div>
                 </div>
             </div>
             <div className="IconBox">
                 <div className="SEli">
-                    <div><img id="img12" className="SES-img" src="images/HTMLIcon.png" alt="Img" /></div>
+                    <div><img id="img12" className="SES-img" src="images/RoutingIcon.png" alt="Img" /></div>
                     <div className="whiteText">RESTful Routing</div>
                 </div>
             </div>
