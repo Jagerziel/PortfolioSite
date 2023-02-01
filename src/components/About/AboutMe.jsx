2ee765e35a503c1ebcf0bb4f19945af91a2cd353
jagerziel <fromnytosg@gmail.com>
2023-02-01 01:26:51 -0500

    Included placeholder image on aboutme

--- src/components/About/AboutMe.jsx
+++ src/components/About/AboutMe.jsx
@@ -11,7 +11,18 @@ export default function AboutMe() {
     <div className="AboutMe-container">
       <div className="About-Me-Header-container">
         <div className="About-Me-Header">
-            About Me Container
+            About
+        </div>
+        <h1 className="About-Me-Header2">
+            BIOGRAPHY SNAPSHOT
+        </h1>
+        <div className="AboutMe-ContentHolder">
+          <div className="AboutMe-Left">
+            <img className="profile-img" src="images/pic_placeholder.jpg" alt="Img" />
+          </div>
+          <div className="AboutMe-Right">
+            Right
+          </div>
         </div>
       </div>
     </div>
