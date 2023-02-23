f0937523888c1401e40023d404c2d6c82581d001
Jagerziel <fromnytosg@gmail.com>
2023-02-23 13:11:34 -0500

    CSS contact page

--- src/components/Contact/ContactPage.jsx
+++ src/components/Contact/ContactPage.jsx
@@ -10,18 +10,19 @@ export default function ContactPage() {
   // Return Div for About Container
   return (
     <div className="Contact-Page-container">
-      <div className="Contact-Page-Header-container">
-        <div className="Contact-Page-Header">
-            Contact
-        </div>
-        <h1 className="Contact-Page-Header2">
-            CONTACT INFORMATION
-        </h1>
-        <div className="Contact-Page-ContentHolder">
-            <p className="ContactDesc">As a lifelong innovator I'm intrigued by other's approaches, methodologies, principals, and strategies.  I enjoy teaching, learning and I very much welcome the opportunity to collaborate or brainstorm ideas. 
-            </p>
-            <p className="ContactDesc">Click on one of the links below to say hello!</p>
-            <div></div>
+        <div className="Contact-Page-container-overlay">
+            <div className="Contact-Page-Header">
+                Contact
+            </div>
+            <h1 className="Contact-Page-Header2">
+                CONTACT INFORMATION
+            </h1>
+            <div className="Contact-Page-ContentHolder">
+                <p className="ContactDesc">As a lifelong innovator I'm intrigued by other's approaches, methodologies, principals, and strategies.  I enjoy teaching, learning and I very much welcome the opportunity to collaborate or brainstorm ideas. 
+                </p>
+                <p className="ContactDesc">Click on one of the links below to say hello!</p>
+                <div></div>
+
         </div>
       </div>
     </div>
