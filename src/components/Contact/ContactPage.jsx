1fe002d756b98e85f023b694416d588b1d3c4044
Jagerziel <fromnytosg@gmail.com>
2023-02-23 19:27:26 -0500

    Added links to contact page

--- src/components/Contact/ContactPage.jsx
+++ src/components/Contact/ContactPage.jsx
@@ -21,23 +21,30 @@ export default function ContactPage() {
                 <div className="ContactLeft">
                     <div>
                         <div className="ContactLeftBox">
-                            <img src={require("./Icons/LinkedinIcon.png")} className="ContactIcon1"/>
+                            <a href="https://www.linkedin.com/in/ryanehrlich/" target="_blank">
+                                <img src={require("./Icons/LinkedinIcon.png")} className="ContactIcon1"/>
+                            </a>
                             <div>LinkedIn Profile</div>
                         </div>
                         <div className="ContactLeftBox">
-                            <img src={require("./Icons/GithubIcon.png")} className="ContactIcon1"/>
+                            <a href="https://github.com/Jagerziel" target="_blank">    
+                                <img src={require("./Icons/GithubIcon.png")} className="ContactIcon1"/>
+                            </a>
                             <div>Github</div>
+                        </div>
+                        <div className="ContactLeftBox">
+                            <img src={require("./Icons/CVIcon.png")} className="ContactIcon1"/>
+                            <div>Resume</div>
                         </div>
                     </div>
                 </div>
                 <div className="ContactRight">
-                    <p className="ContactDesc">As a lifelong innovator I'm intrigued by other's approaches, methodologies, principals, and strategies.  I enjoy teaching, learning and I very much welcome the opportunity to collaborate or brainstorm ideas. 
+                    <p className="ContactDesc">As a lifelong innovator I'm intrigued by other's approaches, methodologies, principals, and strategies.  I enjoy learning and teaching and I very much welcome the opportunity to collaborate or brainstorm ideas. 
                     </p>
-                    <p className="ContactDesc">Click on one of the links below to say hello!</p>
-
+                    <p className="ContactDesc">Please reach out and say hello!</p>
                 </div>
+            </div>
         </div>
-      </div>
     </div>
   );
 }
