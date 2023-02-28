1fd5e378dfbec36a281ef43ec3b10cfc9a323e6f
Jagerziel <fromnytosg@gmail.com>
2023-02-28 18:52:11 -0500

    Fixed bottom bar overflow on small screen sizes

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -9,8 +9,13 @@ export default function BottomBar() {
     // Return Div for BottomBar Container
     return (
         <div className="BottomBar-container">
+            {/* Shows on Webpage and Larger Screens */}
             <div className="BottomBar-text">
             Software Engineer   •   Innovator   •   Fullstack Developer    •    Project Manager    •    Multinational Experience
+            </div>
+            {/* Shows only on small screen sizes */}
+            <div className="BottomBar-text-small">
+            Software Engineer   •   Innovator   •   Project Manager 
             </div>
         </div>
     );
